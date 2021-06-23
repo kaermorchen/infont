4acef84d62d8059a8b3845635da15171a4edd012
Stanislav Romanov <kaermorchen@gmail.com>
2021-06-23 11:36:53 +0300

    Added more settings for jest

--- jest.config.js
+++ jest.config.js
@@ -114,7 +114,7 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  // rootDir: undefined,
+  rootDir: './tests',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
@@ -146,10 +146,9 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+    "**/*.js"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
--- tests/parse.js
+++ tests/parse.js
@@ -3,4 +3,5 @@ import { parse } from '../src/index';
 
 test('it works', () => {
   expect(parse(1, 1)).toBe(2);
+  expect(parse(2, 1)).not.toBe(2);
 });
